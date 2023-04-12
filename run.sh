13bbee203bb0b42b7b62956fbed8dc3da18584bf
Vuk Mirovic <contact@vukmirovic.org>
2023-04-13 01:11:06 +0200

    Sway

--- run.sh
+++ run.sh
@@ -1,66 +1,64 @@
 #!/usr/bin/env bash
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
 echo_sleep () { echo "$1"; sleep 1; }
+ai () { sudo apt install --no-install-recommends --purge "$@"; }
+gpgd () { sudo gpg --dearmour -o "$@"; }
 
 id=$(grep ^ID= /etc/os-release)
 DISTRO=${id/ID=/}
 
-INSTALL_FONTS="fonts-noto-cjk fonts-noto-core fonts-liberation fonts-noto-color-emoji"
-INSTALL_GNOME="gnome-shell-extension-appindicator gnome-shell-extension-bluetooth-quick-connect gnome-tweaks gstreamer1.0-vaapi libavif-gdk-pixbuf"
-INSTALL_GUI="gimp meld mpv"
-INSTALL_UTILS="apt-transport-https curl ffmpeg htop imagemagick lm-sensors ncdu neofetch powertop qemu-system-x86 qemu-system-gui qemu-utils radeontop ranger rsync samba tlp yt-dlp unattended-upgrades powerstat linux-cpupower"
+INSTALL_FONTS="fonts-noto-cjk fonts-noto-core fonts-liberation fonts-noto-color-emoji
+fonts-dejavu-core"
+INSTALL_GUI="gimp gtk2-engines-pixbuf meld mpv mesa-va-drivers mate-calc mousepad ristretto
+webp-pixbuf-loader xarchiver zip unzip zathura libreoffice-gtk3 libreoffice-writer
+libreoffice-calc libreoffice-impress transmission-gtk exfalso python3-musicbrainzngs"
+INSTALL_UTILS="apt-transport-https curl ffmpeg htop imagemagick lm-sensors ncdu
+neofetch powertop qemu-system-x86 qemu-system-gui qemu-utils radeontop ranger rsync
+samba tlp yt-dlp unattended-upgrades upower rclone"
 INSTALL_DEV="docker.io docker-compose git gitk"
-INSTALL_BUILD="build-essential zlib1g-dev libbz2-dev libncurses-dev libffi-dev libreadline-dev libssl-dev libsqlite3-dev liblzma-dev"
-INSTALL_EXTRA="brave-browser viber code signal-desktop nodejs asdf-vm beekeeper-studio dropbox nicotine google-chrome-stable"
-INSTALL_PACKAGES="amd64-microcode $INSTALL_FONTS $INSTALL_GNOME $INSTALL_GUI $INSTALL_UTILS $INSTALL_DEV $INSTALL_BUILD"
-
-REMOVE_GNOME="baobab cheese evolution-data-server fwupd gnome-calendar gnome-characters gnome-clocks gnome-font-viewer gnome-games gnome-logs gnome-maps gnome-music gnome-online-accounts gnome-shell-extensions gnome-software gnome-sound-recorder gnome-sushi gnome-system-monitor gnome-weather ibus totem yelp"
-REMOVE_GAMES="aisleriot gnome-mahjongg gnome-mines gnome-sudoku"
-REMOVE_SYSTEM="plymouth low-memory-monitor snapd systemd-oomd"
-REMOVE_APPS="deja-dup remmina shotwell simple-scan synaptic thunderbird"
-REMOVE_LIBREOFFICE_EXTRAS="libreoffice-help-en-us mythes-en-us hyphen-en-us"
-REMOVE_PACKAGES="$REMOVE_SYSTEM $REMOVE_GNOME $REMOVE_GAMES $REMOVE_APPS $REMOVE_LIBREOFFICE_EXTRAS"
+INSTALL_EXTRA="viber code signal-desktop nodejs asdf-vm beekeeper-studio
+nicotine google-chrome-stable firefox dropbox"
+INSTALL_SWAY_BASE="sway foot waybar swayidle swaylock wofi mako-notifier kanshi
+xdg-desktop-portal-wlr grim slurp jq brightnessctl brightness-udev gammastep
+thunar thunar-archive-plugin tumbler pavucontrol cmus cmus-plugin-ffmpeg ncal"
+INSTALL_SWAY_DESKTOP="pipewire-audio rtkit network-manager xwayland gvfs gvfs-backends eject dconf-cli
+gnome-keyring gnome-icon-theme playerctl libdbus-glib-1-2"
+INSTALL_PACKAGES="$INSTALL_FONTS $INSTALL_GUI $INSTALL_UTILS
+$INSTALL_DEV $INSTALL_SWAY_BASE $INSTALL_SWAY_DESKTOP"
 
 ENABLE_SERVICES="tlp"
-
-DISABLE_PREINSTALLED="avahi-daemon bolt cups cups-browsed ModemManager switcheroo-control"
-if [ "$DISTRO" = "ubuntu" ]; then
-  DISABLE_PREINSTALLED="$DISABLE_PREINSTALLED kerneloops rsyslog"
-fi
-DISABLE_INSTALLED="containerd docker nmbd smbd"
-DISABLE_SERVICES="$DISABLE_PREINSTALLED $DISABLE_INSTALLED"
-
-DISABLE_GNOME="org.gnome.SettingsDaemon.Color org.gnome.SettingsDaemon.PrintNotifications org.gnome.SettingsDaemon.Sharing org.gnome.SettingsDaemon.Smartcard org.gnome.SettingsDaemon.Wacom tracker-miner-fs-3"
-DISABLE_GVFS="gvfs-afc-volume-monitor gvfs-goa-volume-monitor gvfs-gphoto2-volume-monitor"
-DISABLE_USER_SERVICES="$DISABLE_GNOME $DISABLE_GVFS"
+DISABLE_SERVICES="NetworkManager-wait-online containerd docker nmbd smbd"
+DISABLE_USER_SERVICES="gvfs-afc-volume-monitor gvfs-goa-volume-monitor gvfs-gphoto2-volume-monitor"
 
 USER_GROUPS="docker"
-CRON="0 11,17,23 * * * bash -ic backup"
+CRON="0 17,23 * * * bash -ic backup"
 FSTAB="LABEL=PODACI /mnt/PODACI ext4 rw,noatime,x-gvfs-show 0 1"
 DOTFILES_GITHUB="wooque/dotfiles"
 
 brave-browser () {
-  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"| sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-  sudo apt update && sudo apt install brave-browser
+  local key=/usr/share/keyrings/brave-browser-archive-keyring.gpg
+  sudo wget https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg -O $key
+  echo "deb [signed-by=$key arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" \
+    | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  sudo apt update && ai brave-browser
 }
 
 viber () {
+  ai libgstreamer-plugins-bad1.0-0 libopengl0
   wget "https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb" -O /tmp/viber.deb
-  sudo apt install /tmp/viber.deb
+  ai /tmp/viber.deb
 }
 
 code () {
   wget "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" -O /tmp/code.deb
-  sudo apt install /tmp/code.deb
+  ai /tmp/code.deb
 }
 
 asdf-vm () {
-  version="0.11.2"
+  local version="0.12.0"
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v${version}
   . "$HOME/.asdf/asdf.sh"
   asdf plugin-add nodejs
@@ -68,57 +66,78 @@ asdf-vm () {
 }
 
 dropbox () {
-  version="2022.12.05"
-  wget --header "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0" "https://linux.dropbox.com/packages/ubuntu/dropbox_${version}_amd64.deb" -O /tmp/dropbox.deb
-  sudo apt install /tmp/dropbox.deb
-  sudo apt update && sudo apt upgrade
-  apt-key export 5044912E | sudo gpg --dearmour -o /usr/share/keyrings/dropbox.gpg
-  sudo sed -i 's/arch=i386,amd64]/arch=i386,amd64 signed-by=\/usr\/share\/keyrings\/dropbox.gpg]/' /etc/apt/sources.list.d/dropbox.list
+  local version="2022.12.05"
+  wget --header "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:102.0) Gecko/20100101 Firefox/102.0" \
+    "https://linux.dropbox.com/packages/ubuntu/dropbox_${version}_amd64.deb" -O /tmp/dropbox.deb
+  ai /tmp/dropbox.deb
+
+  sudo apt update || true
+  sudo apt upgrade
+
+  local key=/usr/share/keyrings/dropbox.gpg
+  apt-key export 5044912E | gpgd $key
+  sudo sed -i "s/arch=i386,amd64]/arch=i386,amd64 signed-by=${key//\//\\\/}]/" \
+    /etc/apt/sources.list.d/dropbox.list
 }
 
 nodejs () {
-  wget -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
-  echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_18.x bookworm main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-  sudo apt update && sudo apt install nodejs
+  local version=18
+  local key=/usr/share/keyrings/nodesource.gpg
+  wget -O- https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpgd $key
+  echo "deb [signed-by=$key] https://deb.nodesource.com/node_$version.x bookworm main" \
+    | sudo tee /etc/apt/sources.list.d/nodesource.list
+  sudo apt update && ai nodejs
+  sudo npm -g install yarn
 }
 
 signal-desktop () {
-  wget -O- https://updates.signal.org/desktop/apt/keys.asc | sudo gpg --dearmor -o /usr/share/keyrings/signal-desktop-keyring.gpg
-  echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' | sudo tee /etc/apt/sources.list.d/signal-xenial.list
-  sudo apt update && sudo apt install signal-desktop
+  local key=/usr/share/keyrings/signal-desktop-keyring.gpg
+  wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpgd $key
+  echo "deb [arch=amd64 signed-by=$key] https://updates.signal.org/desktop/apt xenial main" \
+    | sudo tee /etc/apt/sources.list.d/signal-xenial.list
+  sudo apt update && ai signal-desktop
 }
 
 beekeeper-studio () {
-  curl https://deb.beekeeperstudio.io/beekeeper.key | sudo gpg --dearmor -o /usr/share/keyrings/beekeeper-studio.gpg
-  echo "deb [signed-by=/usr/share/keyrings/beekeeper-studio.gpg] https://deb.beekeeperstudio.io stable main" | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
-  sudo apt update && sudo apt install beekeeper-studio
+  local key=/usr/share/keyrings/beekeeper-studio.gpg
+  wget -O- https://deb.beekeeperstudio.io/beekeeper.key | gpgd $key
+  echo "deb [signed-by=$key] https://deb.beekeeperstudio.io stable main" \
+    | sudo tee /etc/apt/sources.list.d/beekeeper-studio-app.list
+  sudo apt update && ai beekeeper-studio
 }
 
 nicotine () {
-  curl "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x6E60F93DCD3E27CBE2F0CCA16CEB6050A30E5769" | sudo gpg --dearmor  -o /etc/apt/trusted.gpg.d/nicotine-team-ubuntu-stable.gpg
-  echo "deb https://ppa.launchpadcontent.net/nicotine-team/stable/ubuntu jammy main" | sudo tee /etc/apt/sources.list.d/nicotine-team-ubuntu-stable.list
-  sudo apt update && sudo apt install nicotine
+  local key=/usr/share/keyrings/nicotine-team-ubuntu-stable.gpg
+  wget -O- "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x6E60F93DCD3E27CBE2F0CCA16CEB6050A30E5769" \
+    | gpgd $key
+  echo "deb [signed-by=$key] https://ppa.launchpadcontent.net/nicotine-team/stable/ubuntu jammy main" \
+    | sudo tee /etc/apt/sources.list.d/nicotine-team-ubuntu-stable.list
+  sudo apt update && ai nicotine
 }
 
 google-chrome-stable () {
   wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O /tmp/chrome.deb
-  sudo apt install /tmp/chrome.deb
+  ai /tmp/chrome.deb
+}
+
+firefox () {
+  wget "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" -O /tmp/firefox.tar.bz2
+  tar xvf /tmp/firefox.tar.bz2
+  sudo mv ./firefox /opt
+  sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
+  sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
+  sudo sed -i "s/=firefox/=\/opt\/firefox\/firefox-bin/" /usr/local/share/applications/firefox.desktop
 }
 
 main () {
   echo_sleep "Install packages..."
-  sudo apt install --no-install-recommends --purge $INSTALL_PACKAGES
+  ai $INSTALL_PACKAGES
+  sudo dpkg-reconfigure unattended-upgrades
 
-  for app in $INSTALL_EXTRA; do
-    echo_sleep "Install $app..."
-    $app
-  done
-
-  echo_sleep "Remove packages..."
-  # workaround so snap removal doesn't fail
-  [ -d /var/snap/firefox/common/host-hunspell ] && sudo umount /var/snap/firefox/common/host-hunspell
-  sudo apt purge --auto-remove $REMOVE_PACKAGES
-  [ -d "$HOME"/snap ] && rm -rf "$HOME"/snap
+  echo_sleep "Fix network..."
+  if [ -f /etc/network/interfaces ]; then
+    sudo rm /etc/network/interfaces
+  fi
 
   echo_sleep "Add groups..."
   sudo usermod -a -G "$USER_GROUPS" "$USER"
@@ -134,7 +153,9 @@ main () {
 
   echo_sleep "Update GRUB..."
   sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-  sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="quiet loglevel=3 systemd.show_status=false mitigations=off amd_iommu=off nowatchdog"/' /etc/default/grub
+  GRUB_OPTS="quiet loglevel=3 systemd.show_status=false mitigations=off amd_iommu=off nowatchdog"
+  sudo sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT=\"${GRUB_OPTS//\./\\.}\"/" \
+    /etc/default/grub
   echo 'GRUB_BACKGROUND=""' | sudo tee -a /etc/default/grub
   sudo update-grub
 
@@ -145,11 +166,16 @@ main () {
   echo_sleep "Setup systemd tweaks..."
   sudo sed -i 's/#SystemMaxUse=/SystemMaxUse=10M/' /etc/systemd/journald.conf
   sudo sed -i 's/#RuntimeMaxUse=/RuntimeMaxUse=10M/' /etc/systemd/journald.conf
+  sudo sed -i 's/#DefaultTimeoutStopSec=.*/DefaultTimeoutStopSec=5s/' /etc/systemd/system.conf
+  sudo sed -i 's/#DefaultTimeoutStopSec=.*/DefaultTimeoutStopSec=5s/' /etc/systemd/user.conf
+  sudo sed -i 's/#KillUserProcesses=.*/KillUserProcesses=yes/' /etc/systemd/logind.conf
 
   echo_sleep "Disable Bluetooth hardware volume..."
   sudo mkdir -p /etc/wireplumber/bluetooth.lua.d
-  sudo cp /usr/share/wireplumber/bluetooth.lua.d/50-bluez-config.lua /etc/wireplumber/bluetooth.lua.d
-  sudo sed -i 's/\-\-\["bluez5.enable\-hw\-volume"\] = true/\["bluez5.enable\-hw\-volume"\] = false/' /etc/wireplumber/bluetooth.lua.d/50-bluez-config.lua
+  sudo cp /usr/share/wireplumber/bluetooth.lua.d/50-bluez-config.lua \
+    /etc/wireplumber/bluetooth.lua.d
+  sudo sed -i 's/\-\-\["bluez5.enable\-hw\-volume"\] = true/\["bluez5.enable\-hw\-volume"\] = false/' \
+    /etc/wireplumber/bluetooth.lua.d/50-bluez-config.lua
 
   echo_sleep "Setup TLP..."
   sudo cp ./tlp.conf /etc/tlp.d/custom.conf
@@ -181,8 +207,12 @@ main () {
   dconf load / < "./$DISTRO/dconf.conf"
 
   echo_sleep "Setup autologin..."
-  sudo sed -i "s/#  AutomaticLoginEnable/AutomaticLoginEnable/" /etc/gdm3/daemon.conf
-  sudo sed -i "s/#  AutomaticLogin = user1/AutomaticLogin = $USER/" /etc/gdm3/daemon.conf
+  sudo mkdir /etc/systemd/system/getty@tty1.service.d
+  sudo tee /etc/systemd/system/getty@tty1.service.d/autologin.conf <<EOF
+[Service]
+ExecStart=
+ExecStart=-/usr/sbin/agetty -o '-p -f -- \\u' --skip-login --nonewline --noissue  --noclear --autologin $USER %I \$TERM
+EOF
 
   echo_sleep "Fetch dotfiles..."
   cd "/home/$USER"
@@ -192,11 +222,11 @@ main () {
   git reset --hard origin/master
   git remote set-url origin "git@github.com:$DOTFILES_GITHUB.git"
 
-  echo_sleep "Remove default directories..."
-  find "$HOME" -mindepth 1 -maxdepth 1 -type d -not -path "$HOME/.*" -exec rm -rf {} \;
-
-  echo_sleep "Reset app grid..."
-  gsettings set org.gnome.shell app-picker-layout "[]"
+  # flaky installs at the end
+  for app in $INSTALL_EXTRA; do
+    echo_sleep "Install $app..."
+    $app
+  done
 
   echo_sleep "asdf install..."
   . "$HOME/.asdf/asdf.sh"
