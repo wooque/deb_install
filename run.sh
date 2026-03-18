414b8e2785b509ebe9c1432238207f1906c52fc8
Vuk Mirovic <contact@vukmirovic.org>
2026-03-18 20:39:23 +0100

    upgrade to Node 24

--- run.sh
+++ run.sh
@@ -242,7 +242,7 @@ nodejs () {
   wget -qO- https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpgd $key
   sudo tee /etc/apt/sources.list.d/nodesource.sources <<EOF
 Types: deb
-URIs: https://deb.nodesource.com/node_22.x/
+URIs: https://deb.nodesource.com/node_24.x/
 Suites: nodistro
 Components: main
 Signed-By: $key
