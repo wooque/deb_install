3784e20eb2208e05ae4817c0b120e2a3eae3f4ef
Vuk Mirovic <contact@vukmirovic.org>
2026-05-03 14:56:50 +0200

    remove frequently changing data from dconf

--- update-data.sh
+++ update-data.sh
@@ -4,4 +4,4 @@ DISTRO=$(sed -n 's/^ID=//p' /etc/os-release)
 
 apt list --manual-installed | cut -d / -f 1 | tail -n +2 | sort > "$DISTRO"/manual-installed.txt
 apt list --installed | cut -d / -f 1 | tail -n +2 | sort > "$DISTRO"/installed.txt
-dconf dump / > "$DISTRO"/dconf.conf
+dconf dump / | grep -vE '^(recent-connections|width=uint32|size=\(|window-properties=\[)' > "$DISTRO"/dconf.conf
