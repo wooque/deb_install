f4b16f8bb9308c9cb3df3d58dea30697a21de28f
Vuk Mirovic <contact@vukmirovic.org>
2026-05-10 18:18:41 +0200

    clean dconf

--- update-data.sh
+++ update-data.sh
@@ -4,4 +4,9 @@ DISTRO=$(sed -n 's/^ID=//p' /etc/os-release)
 
 apt list --manual-installed | cut -d / -f 1 | tail -n +2 | sort > "$DISTRO"/manual-installed.txt
 apt list --installed | cut -d / -f 1 | tail -n +2 | sort > "$DISTRO"/installed.txt
-dconf dump / | grep -vE '^(recent-connections|size=\(|window-properties=\[|state=|width=|height=)' > "$DISTRO"/dconf.conf
+dconf dump / | \
+sed '/^\[org\/blueman\/plugins\/autoconnect\]/,/^$/d' | \
+sed '/^\[org\/blueman\/plugins\/recentconns\]/,/^$/d' | \
+sed '/^\[org\/gnome\/nm-applet\/eap/,/^$/d' | \
+sed '/^\[org\/gnome\/gitg\/state/,/^$/d' | \
+grep -vE '^(size=\(|window-properties=\[|state=|width=|height=)' > "$DISTRO"/dconf.conf
